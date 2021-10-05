de768b29e9aaa5cec8d2d580b7c722a3fc477e3a
LoxZdox <gidropony1905@gmail.com>
2021-10-05 22:35:27 +0300

    Just buttons added

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,27 @@ namespace winforms
             button1.Click += button1_Click;
             button2.Click += button2_Click;
             button3.Click += button3_Click;
+            button4.Click += button4_Click;
+            button5.Click += button5_Click;
+            button6.Click += button6_Click;
+            button7.Click += button7_Click;
+            button8.Click += button8_Click;
+            button9.Click += button9_Click;
+            button10.Click += button10_Click;
+            button11.Click += button11_Click;
+            button12.Click += button12_Click;
+            button13.Click += button13_Click;
+            button14.Click += button14_Click;
+            button15.Click += button15_Click;
+            button16.Click += button16_Click;
+            button17.Click += button17_Click;
+            button18.Click += button18_Click;
+            button19.Click += button19_Click;
+            button20.Click += button20_Click;
+            button21.Click += button21_Click;
+            button22.Click += button22_Click;
+            button23.Click += button23_Click;
+            button24.Click += button24_Click;
         }
         private void button1_Click(object sender, EventArgs e){
             button1.Text = "Wtf, stop pushin` me!";
@@ -27,6 +48,51 @@ namespace winforms
         }
         private void button3_Click(object sender, EventArgs e){
             button3.Text = "Wtf, stop pushin` me!";
+        }
+        private void button4_Click(object sender, EventArgs e){
+            button1.Text = "Wtf, stop pushin` me!";
+        }
+        private void button5_Click(object sender, EventArgs e){
+            button2.Text = "Wtf, stop pushin` me!";
+        }
+        private void button6_Click(object sender, EventArgs e){
+            button3.Text = "Wtf, stop pushin` me!";
+        }
+        private void button7_Click(object sender, EventArgs e){
+        }
+        private void button8_Click(object sender, EventArgs e){
+        }
+        private void button9_Click(object sender, EventArgs e){
+        }
+        private void button10_Click(object sender, EventArgs e){
+        }
+        private void button11_Click(object sender, EventArgs e){
+        }
+        private void button12_Click(object sender, EventArgs e){
+        }
+        private void button13_Click(object sender, EventArgs e){
+        }
+        private void button14_Click(object sender, EventArgs e){
+        }
+        private void button15_Click(object sender, EventArgs e){
+        }
+        private void button16_Click(object sender, EventArgs e){
+        }
+        private void button17_Click(object sender, EventArgs e){
+        }
+        private void button18_Click(object sender, EventArgs e){
+        }
+        private void button19_Click(object sender, EventArgs e){
+        }
+        private void button20_Click(object sender, EventArgs e){
+        }
+        private void button21_Click(object sender, EventArgs e){
+        }
+        private void button22_Click(object sender, EventArgs e){
+        }
+        private void button23_Click(object sender, EventArgs e){
+        }
+        private void button24_Click(object sender, EventArgs e){
         }
     }
 }
